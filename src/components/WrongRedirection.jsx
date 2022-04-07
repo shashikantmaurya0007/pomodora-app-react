122707b3df84b6e4c4feb4ea39242d36de84a373
shashikantmaurya0007 <80745766+shashikantmaurya0007@users.noreply.github.com>
2022-04-07 22:53:09 +0530

    refactor:log changes

--- src/components/WrongRedirection.jsx
+++ src/components/WrongRedirection.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const WrongRedirection = () => {
   return (
     <div className="wrongDerection">
-      <h1>404| Page Not Found</h1>
+      <h1>404| Page not Found</h1>
     </div>
   );
 };
